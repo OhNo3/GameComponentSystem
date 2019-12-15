991015cc308aa929fc4f6f10638c4e6f6c61c197
OhNo3 <41865015+OhNo3@users.noreply.github.com>
2019-12-15 23:58:57 +0900

    fix

--- GameComponentSystem/GameObjectManager.cpp
+++ GameComponentSystem/GameObjectManager.cpp
@@ -2,20 +2,25 @@
 
 void GameObjectManager::InitAll(void)
 {
+
 }
 
 void GameObjectManager::UninitAll(void)
 {
+
 }
 
 void GameObjectManager::InputAll(void)
 {
+
 }
 
 void GameObjectManager::UpdateAll(void)
 {
+
 }
 
 void GameObjectManager::DrawAll(void)
 {
+
 }
